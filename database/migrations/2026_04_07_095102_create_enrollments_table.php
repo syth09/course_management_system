3b4c4e26974e1ca987f4692c633eed1c6a2b4c9c
Johann <tatuanphong616@gmail.com>
2026-04-07 17:02:57 +0700

    Create model and migrate table data

---
+++ database/migrations/2026_04_07_095102_create_enrollments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->date('enrolled_at')->default(now());
+            $table->timestamps();
+
+            $table->unique(['course_id', 'student_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
